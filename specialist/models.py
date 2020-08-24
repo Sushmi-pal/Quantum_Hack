36058933fa8b8a0fddd4bdc08fdeb83c41e6afff
Sushmita <sushmipalikhe97@gmail.com>
2020-08-24 17:58:53 +0545

    Adding fields to specialist model

--- specialist/models.py
+++ specialist/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 # Create your models here.
 class doctor_profile(models.Model):
-    name=models.CharField(max_length=20)
+    name=models.CharField(max_length=50)
     location=models.CharField(max_length=20)
     fee=models.IntegerField()
     identification_image=models.ImageField()
     desc=models.CharField(max_length=100)
+    nmc_num = models.CharField(max_length=50,default=' ')
+    phone = models.CharField(max_length=50,default=' ')
+    qualification = models.CharField(max_length=50,default=' ')
+    speciality = models.CharField(max_length=50,default=' ')
 
     def __str__(self):
         return self.name
