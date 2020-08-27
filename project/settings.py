bb2963410118b60066482267e00ed90c5a7f0301
Sushmita <sushmipalikhe97@gmail.com>
2020-08-27 11:26:51 +0545

    extracting symptom's name from file

--- project/settings.py
+++ project/settings.py
@@ -30,7 +30,8 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'specialist.apps.SpecialistConfig',
     'testimonials',
-    'contact'
+    'contact',
+    'disease',
 
 
 ]
@@ -72,8 +73,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'predict',
+        'USER':'postgres',
+        'PASSWORD':'major',
+        'HOST':'localhost'
     }
 }
 
