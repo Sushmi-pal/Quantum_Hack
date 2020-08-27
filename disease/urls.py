b64fbd1a638872be0a094f244f0507dc014d4588
Sushmita <sushmipalikhe97@gmail.com>
2020-08-27 19:55:51 +0545

    Link pages

--- disease/urls.py
+++ disease/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import SearchDisease,checkdisease
 urlpatterns=[
     path('disease/',SearchDisease,name='disease'),
-    path('checkdisease/',checkdisease)
+    path('checkdisease/',checkdisease,name='checkdisease')
 ]
